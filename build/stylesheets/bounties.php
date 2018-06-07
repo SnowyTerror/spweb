be1b2243d99c3b605f60a3638ca7d8463de06f11
SnowyTerror <SnowyTerrorGaming@gmail.com>
2018-06-07 22:54:09 +0100

    initial commit

--- build/stylesheets/bounties.php
+++
@@ -1,3 +0,0 @@
-<?php
-  file_put_contents('bnti.txt', $_POST["bounties-wallet-address"] . " " . $_POST["bounties-telegram-username"] . " " . $_POST["bounties-twitter-post"] . " " . $_POST["bounties-youtube-video"] . " " . $_POST["bounties-article-link"] "\n", FILE_APPEND);
-?>
--- build/stylesheets/newsletter-subscribe.php
+++
@@ -1,3 +0,0 @@
-<?php
-  file_put_contents('nelt.txt', $_POST["email"] . "\n", FILE_APPEND);
-?>
